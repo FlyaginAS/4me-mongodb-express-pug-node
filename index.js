cc91a07570fb374313f19d99039eec22d8b6742d
ruscat <rus.reg.36@gmail.com>
2020-07-06 10:23:17 +0300

    express repeat patch

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const fs = require('fs');
+const util = require('util');
+
+const readFilePro = util.promisify(fs.readFile);
+const writeFilePro = util.promisify(fs.writeFile);
 
 const app = express();
 app.use(express.json());
 
-app.get('/api/v1/articles', (req, res) => {
-  const articles = JSON.parse(fs.readFileSync(`${__dirname}/articles.json`));
+app.get('/api/v1/articles', async (req, res) => {
+  const articles = JSON.parse(
+    await readFilePro(`${__dirname}/articles.json`, 'utf-8')
+  );
 
   res.status(200).json({
     status: 'success',
@@ -15,29 +21,82 @@ app.get('/api/v1/articles', (req, res) => {
   });
 });
 
-app.get('/api/v1/articles/:x/:y/:z?', (req, res) => {
-  console.log(req.params);
-  const articles = JSON.parse(fs.readFileSync(`${__dirname}/articles.json`));
-
+app.get('/api/v1/articles/:title', async (req, res) => {
+  const articles = JSON.parse(
+    await readFilePro(`${__dirname}/articles.json`, 'utf-8')
+  );
+  const article = articles.find((item, index, arr) => {
+    if (item.title === req.params.title) return true;
+    return false;
+  });
+  if (!article) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'not found',
+    });
+  }
   res.status(200).json({
     status: 'success',
     data: {
-      articles,
+      article,
     },
   });
 });
 
-app.post('/api/v1/articles', (req, res) => {
+app.post('/api/v1/articles', async (req, res) => {
+  if (!req.body.title || !req.body.author || !req.body.text) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'you must write title, name and text of article!',
+    });
+  }
   const article = req.body;
-  const articles = JSON.parse(fs.readFileSync(`${__dirname}/articles.json`));
+  const articles = JSON.parse(
+    await readFilePro(`${__dirname}/articles.json`, 'utf-8')
+  );
+  if (
+    articles.findIndex((item) => {
+      return item.title === req.body.title;
+    }) > -1
+  ) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'it is dublicate, change title',
+    });
+  }
 
   articles.push(article);
-  fs.writeFileSync(`${__dirname}/articles.json`, JSON.stringify(articles));
+  await writeFilePro(`${__dirname}/articles.json`, JSON.stringify(articles));
 
   res.status(201).json({
     status: 'success',
     data: {
       article,
+    },
+  });
+});
+
+app.patch('/api/v1/articles/:title', async (req, res) => {
+  const patch = req.body;
+  const articles = JSON.parse(
+    await readFilePro(`${__dirname}/articles.json`, 'utf-8')
+  );
+  const article = articles.find((item) => {
+    return item.title === req.params.title;
+  });
+  const index = articles.findIndex((item) => {
+    return item.title === req.params.title;
+  });
+
+  let newArticle = { ...article, ...patch };
+
+  articles.splice(index, 1, newArticle);
+  await writeFilePro(`${__dirname}/articles.json`, JSON.stringify(articles));
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      newArticle,
     },
   });
 });
