9b84bac22f902d3000d597a0935bd480b8eb9ea3
ruscat <rus.reg.36@gmail.com>
2020-07-08 11:29:19 +0300

    repeat mongoose 085

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+//**************************************************************************
 //connecting to the DB
 const DB = process.env.DB_URL.replace('<password>', `${process.env.DB_PASS}`);
 mongoose
@@ -17,8 +19,43 @@ mongoose
     console.log('DB connection successfull');
   });
 
+//**************************************************************************
+//run server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`App running on port ${port}`);
 });
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'An article must have a title!'],
+    unique: true,
+  },
+  rating: Number,
+  author: {
+    type: String,
+    required: [true, 'An article must have an author!'],
+  },
+  text: {
+    type: String,
+    required: [true, 'An article must have a text'],
+  },
+});
+
+const Article = mongoose.model('Article', articleSchema);
+
+const testArticle = new Article({
+  title: 'Learn HTML',
+  author: 'Alex',
+  text: 'This article about learning html',
+});
+testArticle
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
